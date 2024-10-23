c7356edf86c51b0779fd78e4407201d1a185fddf
chlwoqls <byin0209@naver.com>
2024-10-23 22:53:39 +0900

    feat: Spring Security Config 파일 추가

---
+++ src/main/java/com/prediction/prediction/config/SecurityConfig.java
@@ -0,0 +1,6 @@
+package com.prediction.prediction.config;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+
+public class SecurityConfig extends WebSecurityConfiguration {
+}
