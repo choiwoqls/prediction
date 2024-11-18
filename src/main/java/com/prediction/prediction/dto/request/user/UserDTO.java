b33ee2197b13244880484d282afadf4c942ceb03
chlwoqls <byin0209@naver.com>
2024-11-18 16:01:43 +0900

    feat : 메일 설정

--- src/main/java/com/prediction/prediction/dto/request/user/UserDTO.java
+++ src/main/java/com/prediction/prediction/dto/request/user/UserDTO.java
@@ -3,6 +3,7 @@ package com.prediction.prediction.dto.request.user;
 import com.prediction.prediction.domain.player.Team;
 import com.prediction.prediction.domain.user.User;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -16,7 +17,9 @@ import java.util.Date;
 public class UserDTO {
 
     private Long id;
-    private Team team;
+
+    @NotNull(message = "팀 선택은 필수 입니다.")
+    private Integer team_id;
 
     @NotBlank(message = "이메일은 필수 입력 정보 입니다.")
     @Pattern(regexp = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$", message = "올바른 이메일 형식으로 입력해 주세요.")
@@ -26,8 +29,9 @@ public class UserDTO {
     private String nickname;
 
     @NotBlank(message = "8자 이상 알파벳, 숫자, 특수문자를 포함한 비밀번호를 입력해주세요.")
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\\\d)(?=.*[@$!%*?&])[A-Za-z\\\\d@$!%*?&]{8,}$", message = "8자 이상 알파벳, 숫자, 특수문자를 포함한 비밀번호를 입력해주세요.")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "8자 이상 알파벳, 숫자, 특수문자를 포함한 비밀번호를 입력해주세요.")
     private String password;
+
 
     private int credit;
     private Date date;
@@ -36,7 +40,7 @@ public class UserDTO {
 
     public UserDTO(User user) {
         this.id = user.getId();
-        this.team = user.getTeam();
+        this.team_id = user.getTeam().getId();
         this.email = user.getEmail();
         this.nickname = user.getNickname();
         this.password = user.getPassword();
